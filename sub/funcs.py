dcdda06e7282cfb308f1b24278ab34e2d0d73ef2
NeKot <nekto.nekotov@gmail.com>
2022-07-29 13:30:05 +0200

    added interpretation

--- sub/funcs.py
+++ sub/funcs.py
@@ -4,7 +4,50 @@ With "OP_" prefix.
 """
 from sub.sub import *
 
-OP_ADD = iota(True)
-OP_DEC = iota()
+OP_PUSH = iota()
+OP_POP = iota()
+OP_ADD = iota()
+OP_SUB = iota()
 OP_PRINT = iota()
 FUNCS_SIZE = iota()
+
+
+def PUSH(value: int) -> tuple:
+    return OP_PUSH, value
+
+
+def POP(value: int):
+    return OP_POP, value
+
+
+def ADD():
+    return OP_ADD,
+
+
+def SUB():
+    return OP_SUB,
+
+
+def PRINT():
+    return OP_PRINT,
+
+
+def interpret(commands):
+    stack = []
+    assert FUNCS_SIZE == 5, "Add implementation to interpret"
+    for com in commands:
+        if com[0] == OP_PUSH:
+            stack.append(com[1])
+        elif com[0] == OP_POP:
+            stack.pop()
+        elif com[0] == OP_ADD:
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(a + b)
+        elif com[0] == OP_SUB:
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(b - a)
+        elif com[0] == OP_PRINT:
+            a = stack.pop()
+            print(a)
