b60363a0eeee1fb04143f11325f23efc8c22ba48
NeKot <nekto.nekotov@gmail.com>
2022-07-29 12:58:04 +0200

    added initial functions

---
+++ sub/funcs.py
@@ -0,0 +1,10 @@
+"""
+This should be a list of all functions in programming language.
+With "OP_" prefix.
+"""
+from sub.sub import *
+
+OP_ADD = iota(True)
+OP_DEC = iota()
+OP_PRINT = iota()
+FUNCS_SIZE = iota()
